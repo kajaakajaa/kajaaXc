0c1350e75d5c7deabe77f8334d09fbe71a6bc8a5
kajaakajaa <rwqxh8703@yahoo.co.jp>
2019-09-03 16:35:27 +0900

    edit_routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
-  get 'comments/edit'
-  get 'uploadlists/index'
-  get 'likes/index'
-  get 'likes/show'
-  get 'likes/new'
+  # get 'comments/edit'
+
+  # get 'uploadlists/index'
+  # get 'likes/index'
+  # get 'likes/show'
+  # get 'likes/new'
   # get 'users/index'
   # get 'users/show'
   # get 'users/new'
+
+  resources :likes
+  resources :uploadlists
+  resources :comments
   resources :users
-  get 'songs/index'
-  get 'songs/show'
+  resources :songs
   devise_for :users
+  # get 'songs/index'
+  # get 'songs/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'songs#index'
 
