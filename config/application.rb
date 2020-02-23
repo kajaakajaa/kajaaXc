067d9882c172f0a7decef92bea48429ddf8bf79d
kajaakajaa <rwqxh8703@yahoo.co.jp>
2020-02-23 19:44:27 +0900

    changed_locales/ja.yml

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Xc
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.i18n.default_locale = :ja # デフォルトのロケールを設定
+    config.i18n.default_locale = :en # デフォルトのロケールを設定
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s] # 読み込む辞書ファイルのパスを設定
 
     # コントローラに対応するものしか読み込ませたくない時↓
